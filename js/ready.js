73e021b1b5591505d500e900b2e8f490a0ccc623
Kera Z. Watkins <kzw@acm.org>
2017-06-16 17:04:04 -0400

    adding functionality for mobile navigation

---
+++ js/ready.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $("p").click(function(){
+        $(this).hide();
+    });
+
+// https:// stackoverflow.com/questions/24259016/responsive-menu-show-and-hide-on-click
+
+    $(menu);
+
+});
+
+var menu = function() {
+  var menuVisible = false;
+  $('#menuBtn').click(function() {
+    if (menuVisible) {
+      $('menu-bar').css({'display':'none'});
+      menuVisible = false;
+      return;
+    }
+    $('menu-bar').css({'display':'block'});
+    menuVisible = true;
+  });
+  $('menu-bar').click(function() {
+    $(this).css({'display':'none'});
+    menuVisible = false;
+  });
+}
