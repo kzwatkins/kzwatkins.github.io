1e4cf329c92026981b8bdd49e4d51d484848a25f
Kera Z. Watkins <kzw@acm.org>
2017-06-17 17:03:03 -0400

    get timing right for menu

--- js/ready.js
+++ js/ready.js
@@ -1,26 +1,23 @@
 $(document).ready(function(){
-  var menuVisible = false;
+  var menuVisible = false; // The menu icon is initially showing, and menu is hiding.
 
     $("#menuBtn").click(function(){
-        // $(menu);
         menu(menuVisible);
         menuVisible = !menuVisible;
     });
 
     $(".nav-list").click(function(){
-        // $(menu);
         menu(menuVisible);
         menuVisible = !menuVisible;
     });
 
 });
 
-// Adapted from https:// stackoverflow.com/questions/24259016/responsive-menu-show-and-hide-on-click
-function menu(menuVisible) {
+// (Ultra!) Adapted from https:// stackoverflow.com/questions/24259016/responsive-menu-show-and-hide-on-click
+// If the menu is hiding, then show it.
+// If the menu is showing, then hide it.
 
-  // If the menu is hiding, then show it.
-  // If the menu is showing, then hide it.
-  //  var menuVisible = false;   // The menu is initially hiding.
+function menu(menuVisible) {
 
     if (menuVisible) {
       $('.nav-list').css({'display':'none'});
@@ -34,51 +31,12 @@ function menu(menuVisible) {
 
 function showMenuX (){
     $("#top-bar").css({transform: 'rotate(-45deg)'});
-    $("#mid-bar").hide("fast");
-    // $("#bottom-bar").hide();
-
+    $("#mid-bar").hide();
     $("#bottom-bar").css({transform: 'translateY(-8px) rotate(45deg)'});
   }
 
 function showMenuOrg (){
     $("#top-bar").css({transform: 'rotate(0deg)'});
-    $("#mid-bar").show("fast");
-    // $("#bottom-bar").hide();
-
+    $("#mid-bar").show();
     $("#bottom-bar").css({transform: 'translateY(0px) rotate(0deg)'});
   }
-
-
-  // $('.nav-list').click(function() {
-  //   if (menuVisible) {
-  //     $(this).css({'display':'none'});
-  //     // $('#menuBtn').show();
-  //     showMenuX();
-  //     menuVisible = false;
-  //   } else{
-  //     $(this).css({'display':'block'});
-  //     // $('#menuBtn').hide();
-  //     showMenuOrg();
-  //     menuVisible = true;
-  //   }
-  // });
-
-
-
-
-
-
-
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
-// function openNav() {
-//     $(".nav-list").css({'width' : '250px'});
-//   ("main").css({'marginLeft' : '250px'});
-//     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-// }
-//
-// /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
-// function closeNav() {
-//     document.getElementById("mySidenav").style.width = "0";
-//     document.getElementById("main").style.marginLeft = "0";
-//     document.body.style.backgroundColor = "white";
-// }
