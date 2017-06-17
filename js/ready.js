bc0d15941cbfef1468d93eadb0f86e5b95231467
Kera Z. Watkins <kzw@acm.org>
2017-06-16 22:47:20 -0400

    debugging menu capability

--- js/ready.js
+++ js/ready.js
@@ -12,23 +12,34 @@ $(document).ready(function(){
 
 // Adapted from https:// stackoverflow.com/questions/24259016/responsive-menu-show-and-hide-on-click
 var menu = function() {
-  var menuVisible = false;
-  $('#menuBtn').click(function() {
 
-    if (!menuVisible) {
+  // If the menu is hiding, then show it.
+  // If the menu is showing, then hide it.
+  var menuVisible = false;   // The menu is initially hiding.
+
+  $('#menuBtn').click(function() {
+    if (menuVisible) {
+      $('.nav-list').css({'display':'none'});
+      $(this).show();
+      menuVisible = false;
+    }
+    else{
       $('.nav-list').css({'display':'block'});
       $(this).hide();
       menuVisible = true;
-      return;
     }
-
-    $('.nav-list').css({'display':'none'});
-    $(this).show();
-    menuVisible = false;
   });
 
-  $('.menu-bar').click(function() {
-    $(this).css({'display':'none'});
-    menuVisible = false;
+  $('.nav-list').click(function() {
+    if (menuVisible) {
+      $(this).css({'display':'none'});
+      $('#menuBtn').show();
+      menuVisible = false;
+    } else{
+      $(this).css({'display':'block'});
+      $('#menuBtn').hide();
+      menuVisible = true;
+    }
   });
+
 }
