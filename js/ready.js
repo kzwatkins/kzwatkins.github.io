bf505ff878203d60aa08a7849021bbce30f0f521
Kera Z. Watkins <kzw@acm.org>
2017-06-16 22:33:22 -0400

    tweaking menu still

--- js/ready.js
+++ js/ready.js
@@ -27,8 +27,23 @@ var menu = function() {
     menuVisible = false;
   });
 
-  $('.menu-bar').click(function() {
+  $('.nav-list').click(function() {
     $(this).css({'display':'none'});
+    $('#menuBtn').show();
     menuVisible = false;
   });
+}
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+    $(".nav-list").css({'width' : '250px'});
+  ("main").css({'marginLeft' : '250px'});
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
 }
