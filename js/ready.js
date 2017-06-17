4181517c0bf847118dd0dc8f311d0999b69b82c0
Kera Z. Watkins <kzw@acm.org>
2017-06-16 21:48:23 -0400

    testing jquery functionality

--- js/ready.js
+++ js/ready.js
@@ -1,27 +1,28 @@
 $(document).ready(function(){
-    $("p").click(function(){
-        $(this).hide();
+    $("#menuBtn").click(function(){
+        // $(this).hide();
+        $(".menu-bar").hide();
     });
 
 // https:// stackoverflow.com/questions/24259016/responsive-menu-show-and-hide-on-click
 
-    $(menu);
+    // $(menu);
 
 });
 
-var menu = function() {
-  var menuVisible = false;
-  $('#menuBtn').click(function() {
-    if (menuVisible) {
-      $('menu-bar').css({'display':'none'});
-      menuVisible = false;
-      return;
-    }
-    $('menu-bar').css({'display':'block'});
-    menuVisible = true;
-  });
-  $('menu-bar').click(function() {
-    $(this).css({'display':'none'});
-    menuVisible = false;
-  });
-}
+// var menu = function() {
+//   var menuVisible = false;
+//   $('#menuBtn').click(function() {
+//     if (menuVisible) {
+//       $('menu-bar').css({'display':'none'});
+//       menuVisible = false;
+//       return;
+//     }
+//     $('menu-bar').css({'display':'block'});
+//     menuVisible = true;
+//   });
+//   $('menu-bar').click(function() {
+//     $(this).css({'display':'none'});
+//     menuVisible = false;
+//   });
+// }
