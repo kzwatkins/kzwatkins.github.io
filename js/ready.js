f24a9f547a794034430ebd93b3b70ad79cc733dd
Kera Z. Watkins <kzw@acm.org>
2017-06-16 21:55:39 -0400

    adding functionality to menu

--- js/ready.js
+++ js/ready.js
@@ -1,28 +1,29 @@
 $(document).ready(function(){
     $("#menuBtn").click(function(){
         // $(this).hide();
-        $(".menu-bar").hide();
+        // $(".menu-bar").hide();
+        $(menu);
     });
 
-// https:// stackoverflow.com/questions/24259016/responsive-menu-show-and-hide-on-click
 
-    // $(menu);
+
 
 });
 
-// var menu = function() {
-//   var menuVisible = false;
-//   $('#menuBtn').click(function() {
-//     if (menuVisible) {
-//       $('menu-bar').css({'display':'none'});
-//       menuVisible = false;
-//       return;
-//     }
-//     $('menu-bar').css({'display':'block'});
-//     menuVisible = true;
-//   });
-//   $('menu-bar').click(function() {
-//     $(this).css({'display':'none'});
-//     menuVisible = false;
-//   });
-// }
+// https:// stackoverflow.com/questions/24259016/responsive-menu-show-and-hide-on-click
+var menu = function() {
+  var menuVisible = false;
+  $('#menuBtn').click(function() {
+    if (menuVisible) {
+      $('menu-bar').css({'display':'none'});
+      menuVisible = false;
+      return;
+    }
+    $('menu-bar').css({'display':'block'});
+    menuVisible = true;
+  });
+  $('menu-bar').click(function() {
+    $(this).css({'display':'none'});
+    menuVisible = false;
+  });
+}
