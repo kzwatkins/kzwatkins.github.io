2797f80f0ee6aaab6cd28786707dbd2e254c9d71
Kera Z. Watkins <kzw@acm.org>
2017-06-28 16:53:33 -0400

    Messages updating correctly - fixed width issue

--- js/work4u.js
+++ js/work4u.js
@@ -5,22 +5,23 @@ function getWork4uMsg(position) {
 
 function insertWork4uMsg(position){
   var left = position.left;
-  var width = $("#work4u").width();
+  var width = $(".work4u:first").width();
+  alert(width);
   var offset = 20;
 
   if(left < 0.25 * width + offset){
     // case 0: return "Sorry, US work only!"; // FREEZING!
       // $(".work4umsg").css("color", "white");
-      return "Too far to visit: Remote work only"; // Freezing
+      return "Too Far: Remote Only"; // Freezing
     } else if(left < 0.5 * width + offset){
       // $(".work4umsg").css("color", "black");
-      return "Not too far away: remote work & 1 visit weekly"; // Getting warmer
+      return "Kinda Far: Bi-Monthly Visits"; // Getting warmer
     } else if(left < 0.75 * width + offset){
       // $(".work4umsg").css("color", "white");
-      return "Fairly close by: remote work & 2-3 visits weekly"; // A little warmer
+      return "Kinda Close: Weekly Visits"; // A little warmer
     } else{
       // $(".work4umsg").css("color", "black");
-      return "Neighbors: I'm open for contracts and subcontracts weekdays"; // BOILING HOT!
+      return "Neighbors: Flexible"; // BOILING HOT!
   }
 }
 
